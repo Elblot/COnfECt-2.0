30548226c542385a671ac7a875ea353cb122ac08
Elliott Blot <elliott.blot@gmail.com>
2019-01-16 15:10:51 +0100

    kTail fix

--- CorrelModifClust/src/miners/KTail/KTail.java
+++ CorrelModifClust/src/miners/KTail/KTail.java
@@ -24,6 +24,7 @@ package miners.KTail;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 import miners.FSAminer;
@@ -38,130 +39,133 @@ import fsa.Transition;
 /**
  * KTail miner
  * 
- * @author Sylvain Lamprier
- * modified by Elliott Blot
+ * original @author Sylvain Lamprier
+ * fixed and modified by Elliott Blot
  *
  */
 public class KTail implements FSAminer{
 	private static final long serialVersionUID = 1L;
 	int horizon;
 	String algo;
-	
+
 	public KTail(Integer k, String algo){
 		horizon=k;
 		this.algo = algo;
 	}
-	
-	
+
+
 	public String getName(){
 		return("KTail_k="+horizon);
 	}
-	
+
 	public String toString(){
 		return getName();
 	}
-	
+
 	@Override
 	public FSA transform(ArrayList<Trace> traces) {
-	
 		StateNode root=new StateNode(); 
+		System.out.println(this.toString());
 		HashSet<StateNode> all_states=new HashSet<StateNode>();
 		HashSet<State> initial_states=new HashSet<State>();
 		HashSet<State> final_states=new HashSet<State>();
-		ArrayList<Statement> last_call = new ArrayList<Statement>();
-		ArrayList<Statement> last_return = new ArrayList<Statement>();
-		boolean vide=false;
+		boolean vide = false;
+		boolean mergeinit = true;
+		
 		StateNode source=new StateNode();
 		all_states.add(source);
 		ArrayList<Statement> tInitial=new ArrayList<Statement>();
+		
+		/***** initial state can merge only if it have only one kfuture *****/
+
+		tInitial.add(traces.get(0).getByIndex(1));/// WTF getByIndex begin by 1, it's java!!!!!!
 		if (traces.get(0).getSize() >= 2) {
-			tInitial.add(traces.get(0).getByIndex(1));
 			tInitial.add(traces.get(0).getByIndex(2));
-			for(Trace t:traces){
-				int size=t.getSize();
-				if (size==0){
-					vide=true;
-				}	
-				ArrayList<Statement> tVerif=new ArrayList<Statement>();
-				tVerif.add(t.getByIndex(1));
-				if(t.getSize() >= 2) {
-					tVerif.add(t.getByIndex(2));
-				}
-				else {
-					tVerif.add(new Statement());
-				}
-				if(!tInitial.equals(tVerif)) {
-					tInitial.clear();
-					tInitial.add(new Statement(new Method("nope")));
-					break;
-				}
-			}
+		}
+		else {
+			tInitial.add(traces.get(0).getByIndex(1));
 		}
 		for(Trace t:traces){
-			String str = "";
-			boolean b = false;
-			boolean b3 = false;
-			int c = 0;
+			int size=t.getSize();
+			if (size==0){
+				vide=true;
+			}
+			ArrayList<Statement> tVerif=new ArrayList<Statement>();
+			tVerif.add(t.getByIndex(1));
+			if(t.getSize() >= 2) {
+				tVerif.add(t.getByIndex(2));
+			}
+			else {
+				tVerif.add(t.getByIndex(1));
+			}
+			if(!tInitial.equals(tVerif)) {
+				tInitial.clear();
+				tInitial.add(new Statement(new Method("nope")));
+				mergeinit= false;
+				System.out.println("init mergepas");
+				break;
+			}
+		}
+
+		System.out.println(tInitial.toString());
+		if (mergeinit){
+			source = root.getSetStateNode(new ArrayList<Statement>(tInitial));
+		}
+		for(Trace t:traces){
+			System.out.println("\n\n\n new trace :" + t);
+			String call = "";
 			int size=t.getSize();
 			if (size==0){
 				vide=true;
 			}
 			ArrayList<Statement> thorizon=new ArrayList<Statement>();
+			ArrayList<Statement> callLoop = new ArrayList<Statement>();
 			thorizon.add(new Statement(new Method("initKtail")));
-			Statement st=t.getByIndex(1);
+			callLoop.add(new Statement(new Method("initKtail")));
+			int i = 1;
+			Statement st=t.getByIndex(i);
 			thorizon.add(st);
-			
-			/****************************loop for the initial state*****************************/
-			
-			for(int i=2;((i<=size) && (i<=horizon));i++){
-				st=t.getByIndex(i);
-				switch(algo){
-					case "weak":
-						if (b && c == 0) {
-							thorizon.remove(0);
-							thorizon.remove(0);
-							thorizon.add(t.getByIndex(i+horizon -2));
-							thorizon.add(t.getByIndex(i+horizon -1));
-							b = false;
-						}
-						if (st.toString().contains("call")) {
-							if (c == 1) {
-								thorizon.remove(1);
-								thorizon.add(t.getByIndex(i+horizon -1));
-							}
-							c = 2;
-							i = i + 1;
-							str = st.toString();
-							if (i+horizon-1 < size) {
-								last_call.add(t.getByIndex(i-1));
-								last_return.add(t.getByIndex(i));
-								thorizon.add(new Statement(new Method(str + t.getByIndex(i+1).toString())));
-							}
-							else {
-								last_call.add(t.getByIndex(i-1));
-								last_return.add(t.getByIndex(i));
-								thorizon.add(new Statement(new Method(str)));
-								b3 = true;
-							}
-							continue;
-						}
-						else if (c != 0) {
-							thorizon.add(new Statement(new Method(str + t.getByIndex(i+horizon).toString())));
-							c--;
-						}
-						else {
-							thorizon.add(st);
-						}
-						break;	
-				
-					default :
-						thorizon.add(st);
-						break;	
+			callLoop.add(st);
+			i++;
+			System.out.println("i :" + i);
+			System.out.println("size :" + size);
+			/**********************loop for the initial state*****************/
+			while (thorizon.size() < horizon +1 && i <= size) {
+				System.out.println("in the while");
+				if (t.getByIndex(i).toString().contains("call") || thorizon.get(thorizon.size()-1).toString().contains("return")) {
+					call = t.getByIndex(i).toString().toString();
+					i += 2;
 				}
+				if (!call.equals("")){
+					System.out.println("chat");
+					if(i < size) {
+						st=new Statement(new Method(call + "|||" + t.getByIndex(i).toString()));
+					}
+					else {
+						st=new Statement(new Method(call + "|||"));
+					}
+				}
+				else {
+					st=t.getByIndex(i);
+				}
+				callLoop.add(st);
+				if(i <= size) {
+					thorizon.add(t.getByIndex(i));
+				}
+				else {
+					thorizon.add(t.getByIndex(1));
+				}
+				i++;
+				call = "";
 			}
-			
+			if (thorizon.size() < horizon +1 && i >= size) {
+				thorizon.add(t.getByIndex(1));
+				callLoop.add(t.getByIndex(1));
+			}
+			//System.out.println("\ntinitil: " + tInitial.toString());
+			System.out.println(thorizon);
+			System.out.println("call: " + callLoop.toString());
 			/****************************************************************/
-
 			StateNode last_node = source;
 			if (thorizon.get(0).toString().equals("initKtail")){
 				last_node = source;
@@ -172,121 +176,106 @@ public class KTail implements FSAminer{
 			all_states.add(last_node);
 			initial_states.add(last_node);
 			thorizon.remove(0);
-			boolean b2 = false;
-			b = false;
-			for(int i=1;i<=size;i++){
-				switch(algo){
-					case "weak":
-						if (b3) {
-							b3 = false;
-							i = size;
-						}
-						if (i<=(size-horizon)){
-							st=t.getByIndex(i+horizon);
-							if (b && c == 0) {
-								thorizon.remove(0);
-								thorizon.remove(0);
-								thorizon.add(t.getByIndex(i+horizon -2));
-								thorizon.add(t.getByIndex(i+horizon -1));
-								str="";
-								b = false;
-							}
-							if (st.toString().contains("call")) {
-								if (c == 1) {
-									thorizon.remove(1);
-									thorizon.add(t.getByIndex(i+horizon -1));
-								}
-								c = 2;
-								i = i + 1;
-								str = st.toString();
-								last_call.add(t.getByIndex(i+horizon-1));
-								last_return.add(t.getByIndex(i+horizon));
-								continue;
-							}
-							else if (c != 0) {
-								if (i<=2) {
-									c--;
-									i = i+2;
-									thorizon.remove(1);
-									thorizon.add(t.getByIndex(i+horizon -1));
-									thorizon.add(t.getByIndex(i+horizon ));
-									b = true;
-								}
-								else {
-									thorizon.add(new Statement(new Method(str + t.getByIndex(i+horizon).toString())));
-									c--;
-								}
-							}
-							else {
-								thorizon.add(st);
-							}
-						}
-						else{
-							if (b2) {
-								thorizon.remove(0);
-								thorizon.remove(0);
-								thorizon.add(t.getByIndex(i+horizon -2));
-								thorizon.add(new Statement());
-								b2 = false;						
-							}
-							if (b && c == 0) {
-								thorizon.remove(0);
-								thorizon.remove(0);
-								thorizon.add(t.getByIndex(i+horizon -2));
-								thorizon.add(t.getByIndex(i+horizon -1));
-								str="";
-								b = false;
-							}
-							if (c == 1) {
-								b2 = true;
-								str="";
-							}
-							thorizon.add(new Statement(new Method(str)));
-						}
-						break;
-
-					default:
-						if (i<=(size-horizon)){
-							st=t.getByIndex(i+horizon);
-							thorizon.add(st);
-						}
-						else{
-							thorizon.add(new Statement());
-						}
-						break;
+			callLoop.remove(0);
+			while (i <= size) {
+				if (t.getByIndex(i).toString().contains("call") && !thorizon.get(horizon - 1).toString().contains("return")) {
+					call = t.getByIndex(i).toString();
+					i += 2;
+					if (i <= size) {
+						callLoop.add(new Statement(new Method(call + "|||" + t.getByIndex(i).toString())));
+					}
+					else {
+						callLoop.add(new Statement(new Method(call + "|||")));
+					}
+				}	
+				else {
+					callLoop.add(t.getByIndex(i));
+				}
+				if (i <= size) {
+					st=t.getByIndex(i);
+					thorizon.add(st);
+				}	
+				else {
+					thorizon.add(t.getByIndex(1));
 				}
 				Statement first=thorizon.remove(0);
+				callLoop.remove(0);
 				StateNode stnode =new StateNode();
 				if(thorizon.equals(tInitial)) {
 					stnode = source;
+				}	
+				else {
+					stnode=root.getSetStateNode(new ArrayList<Statement>(callLoop));
+				}	
+				all_states.add(last_node);
+				last_node.addTransition(first,stnode);
+				
+				System.out.println("\nthorizon: " + thorizon.toString());
+				System.out.println("call: " + callLoop.toString());
+				System.out.println("node: " + stnode.toString());
+				if (callLoop.get(0).toString().contains("|||")) {
+					System.out.println("here ");
+					String id = getID(callLoop.get(0).toString());
+					//if (!stnode.getTransitions().toString().contains("call" + id)) {
+					StateNode call_node;
+					//StateNode call_node = new StateNode();
+					ArrayList<Statement> mergecall = new ArrayList<Statement>();
+					mergecall.add(new Statement(new Method("return" + id)));
+					mergecall.add(thorizon.get(0));
+					System.out.println("merge: " + mergecall.toString());
+					call_node = root.getSetStateNode(new ArrayList<Statement>(mergecall));
+					all_states.add(call_node);
+					stnode.addTransition(new Statement(new Method("call"   + id)), call_node);
+					call_node.addTransition(new Statement(new Method("return" + id)), stnode);
+					//}
+				}
+				last_node=stnode;
+				i++;
+			}
+			/***** cyclic: the last state merge with the initial state*****/		
+			for (int j = 1; j<=horizon;j++) {
+				if (size >= j) {
+					thorizon.add(t.getByIndex(j));
+					callLoop.add(t.getByIndex(j));
 				}
 				else {
-					stnode=root.getSetStateNode(new ArrayList<Statement>(thorizon));
+					System.out.println("here "+ j);
+					thorizon.add(t.getByIndex(1));
+					callLoop.add(t.getByIndex(1));
 				}
+				Statement first=thorizon.remove(0);
+				callLoop.remove(0);
+				StateNode stnode =new StateNode();
+				if(thorizon.equals(tInitial) || j==horizon) {
+					stnode = source;
+				}	
+				else {
+					stnode=root.getSetStateNode(new ArrayList<Statement>(callLoop));
+				}	
 				all_states.add(last_node);
-				if (i==size){
-					   final_states.add(stnode);
-					   all_states.add(stnode);
-				}
 				last_node.addTransition(first,stnode);
+				System.out.println("\nthorizon2: " + thorizon.toString());
+				System.out.println("call2: " + callLoop.toString());
+				System.out.println("node2: " + stnode.toString());
+				if (callLoop.get(0).toString().contains("|||")) {
+					String id = getID(callLoop.get(0).toString());
+					//if (!stnode.getTransitions().toString().contains("call" + id)) {
+					StateNode call_node;
+					//StateNode call_node = new StateNode();
+					ArrayList<Statement> mergecall = new ArrayList<Statement>();
+					mergecall.add(new Statement(new Method("return" + id)));
+					mergecall.add(thorizon.get(0));
+					System.out.println("merge: " + mergecall.toString());
+					call_node = root.getSetStateNode(new ArrayList<Statement>(mergecall));
+					all_states.add(call_node);
+					stnode.addTransition(new Statement(new Method("call"   + id)), call_node);
+					call_node.addTransition(new Statement(new Method("return" + id)), stnode);
+					//}
+				}
 				last_node=stnode;
-				if (( b || i >= size) && !last_call.isEmpty()) {
-					String lc = last_call.get(0).toString();
-					if(!last_node.getTransitions().toString().contains(lc)) {	
-						StateNode call_node = new StateNode();
-						all_states.add(call_node);
-						last_node.addTransition(last_call.get(0),call_node);//call
-						call_node.addTransition(last_return.get(0), last_node);//return
-					}
-					last_call.remove(0);
-					last_return.remove(0);
-				}
-				if (c == 1) {
-					b = true;
-				}
 			}
 		}
-		
+
 		FSA fsa=new FSA();
 		fsa.addStates(new ArrayList<State>(all_states));
 		fsa.setInitialState(source);
@@ -304,6 +293,11 @@ public class KTail implements FSAminer{
 			fsa.addTransitions(new ArrayList<Transition>(s.getTransitions()));
 		}
 		return(fsa);
+	}
+
+	static String getID(String trans) {
+		String call = trans.substring(trans.indexOf("_"), trans.indexOf("|"));
+		return call;
 	}
 	
 	static void deleteRecursive(File f)  {
